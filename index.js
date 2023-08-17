35adff0249f51837f5daa373d6d7793b535d0b0e
Jake <zamorejake@gmail.com>
2023-08-17 14:22:56 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the database.`)
+);
+
